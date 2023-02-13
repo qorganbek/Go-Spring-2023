a73362eb736fc4811919e7d529456d5a371e2079
qorganbek <dimashqorganbek@gmail.com>
2023-02-13 17:39:48 +0600

    upd

--- ass_1/data/products.go
+++ ass_1/data/products.go
@@ -9,18 +9,18 @@ type Product struct {
 	ID        int
 	Title     string
 	Price     int
-	ArrRating []int
+	ArrRating []float64
 }
 
-func (p Product) Rating() float32 {
+func (p Product) Rating() float64 {
 	if len(p.ArrRating) == 0 {
 		return 0.0
 	}
-	sum := 0
+	sum := 0.0
 	for i := range p.ArrRating {
 		sum += p.ArrRating[i]
 	}
-	return float32(sum / len(p.ArrRating))
+	return sum / float64(len(p.ArrRating))
 }
 
 func SearchProduct(title string) {
@@ -32,7 +32,7 @@ func SearchProduct(title string) {
 	}
 }
 
-func GiveRating(id int, rate int) error {
+func GiveRating(id int, rate float64) error {
 	ProductDeserializer()
 
 	list, err := ProductDb[id]
@@ -45,4 +45,26 @@ func GiveRating(id int, rate int) error {
 
 	ProductSerializer()
 	return nil
+}
+
+func FilterByRating(lowRating float64, highRating float64) {
+	ProductDeserializer()
+
+	for key, val := range ProductDb {
+		if lowRating < val.Rating() && highRating > val.Rating() {
+			fmt.Println(key, val.Title, val.Rating(), val.Price)
+		}
+	}
+
+}
+
+func FilterByPrice(lowPrice int, highPrice int) {
+	ProductDeserializer()
+
+	for key, val := range ProductDb {
+		if lowPrice < val.Price && highPrice > val.Price {
+			fmt.Println(key, val.Title, val.Rating(), val.Price)
+		}
+	}
+
 }
--- ass_1/main.go
+++ ass_1/main.go
@@ -13,37 +13,43 @@ func showMainMenu() {
 }
 
 func showLogInMenu() {
-	fmt.Println("1. Log Out User")
+	fmt.Println("1. Log Out")
 	fmt.Println("2. Product")
 	fmt.Println("3. Exit")
 }
 
-func ProductMenu2() {
-	fmt.Println("1. Product List")            // +
-	fmt.Println("2. Search Product by title") // +
-	fmt.Println("3. Filter Product")          // -
-	fmt.Println("4. Give rating a Product")   // -
+func ProductMenu() {
+	fmt.Println("1. Product List")
+	fmt.Println("2. Search Product by title")
+	fmt.Println("3. Filter Product")
+	fmt.Println("4. Give rating a Product")
 }
 
-func AdminPanel2() {
+func AdminPanel() {
 	fmt.Println("1. User")
 	fmt.Println("2. Product")
 }
 
-func AdminUser2() {
+func AdminUser() {
 	fmt.Println("1. Create User")
 	fmt.Println("2. Delete User")
 	fmt.Println("3. List User")
 }
 
-func AdminProduct2() {
+func AdminProduct() {
 	fmt.Println("1. Create Product")
 	fmt.Println("2. Delete Product")
 	fmt.Println("3. List Product")
 }
 
+func FilterMenu() {
+	fmt.Println("1. Filter by rating")
+	fmt.Println("2. Filter by Price")
+}
+
 func main() {
 	ok := true
+	id := 1
 	logIn := false
 	var command int
 	for ok {
@@ -51,50 +57,75 @@ func main() {
 			showMainMenu()
 			fmt.Scan(&command)
 			if command == 1 {
-				AdminPanel2()
+				AdminPanel()
 				var adminCommand int
 				fmt.Scan(&adminCommand)
 				if adminCommand == 1 {
-					AdminUser2()
+					AdminUser()
 					var adminUserCommand int
 					fmt.Scan(&adminUserCommand)
 
 					if adminUserCommand == 1 {
+						fmt.Println("If u want to create user enter your username/email and password")
 						var email, password string
 						fmt.Scan(&email, &password)
 						u1 := data.User{Email: email, Password: password}
-						data.Service{}.CreateUser(u1)
+						err := data.Service{}.CreateUser(u1)
+						if err != nil {
+							fmt.Println("New User authorization Failed!")
+						} else {
+							fmt.Println("New User authorization Success!")
+						}
+
 					} else if adminUserCommand == 2 {
+						fmt.Println("If u want to delete account enter your username/email and password")
 						var email, password string
 						fmt.Scan(&email, &password)
 						u1 := data.User{Email: email, Password: password}
-						data.Service{}.DeleteUser(u1)
+						err := data.Service{}.DeleteUser(u1)
+						if err != nil {
+							fmt.Println("User deletion failed!")
+						} else {
+							fmt.Println("User successfully deleted")
+						}
 					} else if adminUserCommand == 3 {
+						fmt.Println("List of users: ")
 						data.Service{}.ListUsers()
 					} else {
 						fmt.Println("Wrong command!")
 					}
 
 				} else if adminCommand == 2 {
-					AdminProduct2()
+					AdminProduct()
 					var adminProductCommand int
 					fmt.Scan(&adminProductCommand)
 
 					if adminProductCommand == 1 {
-						var id, price int
+						fmt.Println("If u want to create new product title, price")
+						var price int
 						var title string
-						fmt.Scan(&id, &title, &price)
+						fmt.Scan(&title, &price)
 						p1 := data.Product{ID: id, Title: title, Price: price}
-						data.Service{}.CreateProduct(p1)
-
+						err := data.Service{}.CreateProduct(p1)
+						id++
+						if err != nil {
+							fmt.Println("Create new product failed!")
+						} else {
+							fmt.Println("New Product successfully created!")
+						}
 					} else if adminProductCommand == 2 {
-						var id, price int
+						var idD, price int
 						var title string
-						fmt.Scan(&id, &title, &price)
-						p1 := data.Product{ID: id, Title: title, Price: price}
-						data.Service{}.DeleteProduct(p1)
-
+						fmt.Scan(&idD, &title, &price)
+						p1 := data.Product{ID: idD, Title: title, Price: price}
+						err := data.Service{}.DeleteProduct(p1)
+						if err != nil {
+							fmt.Println("Product deletion failed!")
+						} else {
+							fmt.Println("Delete product failed!")
+						}
 					} else if adminProductCommand == 3 {
+						fmt.Println("Product List: ")
 						data.Service{}.ListProducts()
 					} else {
 						fmt.Println("Wrong command!")
@@ -138,23 +169,49 @@ func main() {
 			if command == 1 {
 				logIn = false
 			} else if command == 2 {
-				ProductMenu2()
+				ProductMenu()
 				var productCommand int
 				fmt.Scan(&productCommand)
 				if productCommand == 1 {
+					fmt.Println("Product List: ")
 					data.Service{}.ListProducts()
 				} else if productCommand == 2 {
+					fmt.Println("Please enter title if product: ")
 					var title string
 					fmt.Scan(&title)
 					data.SearchProduct(title)
 				} else if productCommand == 4 {
-					var rate, id int
-					fmt.Scan(&id, &rate)
-					data.GiveRating(id, rate)
+					fmt.Println("If u want to give rating to product enter id product and your rating 1-10")
+					var rate float64
+					var idG int
+					fmt.Scan(&idG, &rate)
+					data.GiveRating(idG, rate)
+				} else if productCommand == 3 {
+					FilterMenu()
+					var filterCommand int
+					fmt.Scan(&filterCommand)
+
+					if filterCommand == 1 {
+						fmt.Println("Enter your low rating and high rating")
+						var highRating, lowRating float64
+						fmt.Scan(&lowRating, &highRating)
+						fmt.Println("Products by filter: ")
+						data.FilterByRating(lowRating, highRating)
+					} else if filterCommand == 2 {
+						fmt.Println("Enter your low price and high price")
+						var highPrice, lowPrice int
+						fmt.Scan(&lowPrice, &highPrice)
+						fmt.Println("Products by filter: ")
+						data.FilterByPrice(lowPrice, highPrice)
+					} else {
+						fmt.Println("Wrong command!")
+					}
+
 				} else {
 					fmt.Println("Wrong command!")
 				}
 			} else if command == 3 {
+				fmt.Println("Thanks for using our system!")
 				ok = false
 				break
 			} else {
@@ -162,5 +219,5 @@ func main() {
 			}
 		}
 	}
-
+	fmt.Println("End. Thanks for using our system!")
 }
