6baeb1739d5e18e24eb0afc9765f81c0da505177
qorganbek <dimashqorganbek@gmail.com>
2023-02-21 12:15:13 +0600

    upd

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/qorganbek/Go-Spring-2023
+
+go 1.19
--- http/main.go
+++ http/main.go
@@ -14,15 +14,28 @@ func MainView(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "<h1>Main Page</h1>")
 }
 
-
 // admin panel
 // sign up, sign in
 // product
 
-
 func main() {
-	fmt.Println("Server started at this url http://localhost:8080/")
-	http.HandleFunc("/", MainView)
-	http.HandleFunc("/hello", HelloView)
+	fmt.Println("Server is listening on localhost:8080, open your browser on http://localhost:8080/")
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "About Page")
+	})
+
+	http.HandleFunc("/contact", func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "<h1>Contact Page</h1>")
+	})
+
+	http.HandleFunc("/person", func(w http.ResponseWriter, req *http.Request) {
+		name := req.URL.Query().Get("name")
+		age := req.URL.Query().Get("age")
+		fmt.Fprintf(w, "Name: %s Age: %s", name, age)
+	})
+
 	http.ListenAndServe(":8080", nil)
 }
